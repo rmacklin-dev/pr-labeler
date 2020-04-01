b04c5801cf2219a383520b6a3d7f8ae10f20be5a
Richard Macklin <richard.github@nrm.com>
2020-03-31 18:26:51 -0700

    WIP attempt to fetch existing GitHub team data

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import slugify from '@sindresorhus/slugify'
 
 async function run() {
   try {
@@ -7,15 +8,63 @@ async function run() {
     const teamDataPath = core.getInput('team-data-path')
 
     const client = new github.GitHub(token)
+    const org = github.context.repo.owner
 
     core.debug(`fetching team data from ${teamDataPath}`)
     const teamData: any = await getTeamData(client, teamDataPath)
 
     core.debug(`teamData: ${JSON.stringify(teamData)}`)
+
+    Object.keys(teamData).forEach(async function(teamName) {
+      const teamSlug = slugify(teamName, {decamelize: false})
+      const {existingTeam, existingMembers} = await getExistingTeamAndMembers(
+        client,
+        org,
+        teamSlug
+      )
+
+      if (existingTeam) {
+        core.debug(`existing team members for team slug ${teamSlug}:`)
+        core.debug(JSON.stringify(existingMembers))
+        // update team
+      } else {
+        // create new team
+        core.debug(`No team found in ${org} with slug ${teamSlug}`)
+      }
+    })
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
   }
+}
+
+async function getExistingTeamAndMembers(
+  client: github.GitHub,
+  org: string,
+  teamSlug: string
+): Promise<any> {
+  let existingTeam
+  let existingMembers: string[] = []
+
+  try {
+    const teamResponse = await client.teams.getByName({
+      org,
+      team_slug: teamSlug
+    })
+
+    existingTeam = teamResponse.data
+
+    const membersResponse = await client.teams.listMembersInOrg({
+      org,
+      team_slug: teamSlug
+    })
+
+    existingMembers = membersResponse.data.map(m => m.login)
+  } catch (error) {
+    existingTeam = null
+  }
+
+  return {existingTeam, existingMembers}
 }
 
 async function getTeamData(
